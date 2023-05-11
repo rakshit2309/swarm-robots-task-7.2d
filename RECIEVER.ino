9ed67b889e1c934f461b11f4ae4c8b10b5f55ca2
rakshit2309 <133203471+rakshit2309@users.noreply.github.com>
2023-05-11 16:56:30 +0530

    Add files via upload

---
+++ RECIEVER.ino
@@ -0,0 +1,113 @@
+#include <VirtualWire.h>
+char *data;
+char input;     
+void setup()
+{
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+  Serial.begin(9600);  
+  vw_set_tx_pin (3);
+
+  pinMode(5, LOW);
+  pinMode(6, LOW);
+  pinMode(10, LOW);
+  pinMode(11, LOW);
+
+void loop()
+{
+  // speed = analogRead(A0);
+  // speed = speed * 0.249;
+  // analogWrite(A,speed);
+  // analogWrite(B,speed);
+
+  while(Serial.available())
+  {
+    input =Serial.read();
+    Serial.println(input);
+    
+    if(input == 'F')
+    {
+      data="f";
+      vw_send((uint8_t *)data, strlen (data));
+      vw_wait_tx();
+      forward();
+    }
+
+    else if (input == 'R')
+    {
+      data="r";
+      vw_send((uint8_t *)data, strlen (data));
+      vw_wait_tx();
+    
+      right();
+    }
+
+    else if (input== 'L')
+    {
+      data="l";
+      vw_send((uint8_t *)data, strlen (data));
+      vw_wait_tx();
+      left();    
+    }  
+
+    else if (input== 'G')
+    {
+      data="g";
+      vw_send((uint8_t *)data, strlen (data));
+      vw_wait_tx();
+      backward();
+
+    }  
+    else if (input== 'S')
+    {
+      data="s";
+      vw_send((uint8_t *)data, strlen (data));
+      vw_wait_tx();//STOP (all motors stop)
+      stop();
+    
+    }  
+  }
+}  
+
+void forward()
+  {
+  digitalWrite(6, LOW  );
+  digitalWrite(5, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+}
+
+void stop()
+  {
+  digitalWrite(6, LOW );
+  digitalWrite(5, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+}
+
+void backward()
+  {
+  digitalWrite(6, HIGH  );
+  digitalWrite(5, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+}
+
+void right()
+  {
+  digitalWrite(6, HIGH  );
+  digitalWrite(5, LOW);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+}
+
+void left()
+  {
+  digitalWrite(6, LOW );
+  digitalWrite(5, HIGH);
+  digitalWrite(10, LOW);
+  digitalWrite(11, HIGH);
+}
